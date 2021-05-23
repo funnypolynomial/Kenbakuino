cf0b9c99e7492ef47307aa3be188c5d771ef54e1
Mark Wilson <funnypolynomial@gmail.com>
2021-05-23 15:00:45 +1200

    instruction fix

--- Kenbakuino.ino
+++ Kenbakuino.ino
@@ -15,6 +15,7 @@
 //  May 2019: Added Auto-run program at start-up
 //            Added Clock consts for no RTC or RTC with no SRAM
 //  Jun 2019: Added system extensions to read/write EEPROM
+//  May 2021: Fixed right-shift sign extension and issues with multi-bit roll instructions
 // ==================================================================
 
 // WTF.  We need to include these in the "main sketch" if we want to include them in "tabs"?
